2e7181b15c1d78de958b735bad5216a5737d55aa
Apollon Oikonomopoulos <apollon@noc.grnet.gr>
2009-05-18 18:38:39 +0300

    Removed accepted_langs parsing function

--- wayf/util.py
+++ wayf/util.py
@@ -17,7 +17,7 @@ ipspaces = {
      'aueb.gr':	        ['195.251.255.0/24', '2001:648:2020::/48'],
 
      'auth.gr':	        ['155.207.0.0/16', '192.104.147.0/24', 
-                         '155.207.248.0/21', '2001:648:2800::/48'],
+                         '2001:648:2800::/48'],
 
      'cti.gr':	        ['150.140.0.0/17'],
 
@@ -80,36 +80,6 @@ ipspaces = {
 def urldecode(string):
     urlenc = re.compile(r'%([0-9a-fA-F]{2})')
     return urlenc.sub(lambda x: chr(int(x.group(1), 16)), string)
-
-def acceptedlangs(http_a_l):
-	# "en" is the default language with the lowest weight
-	# however, if defined in our input, it'll be overriden
-	langs = { 'en': 0 }
-
-	# first construct a dict of lang => weight
-	for lang in http_a_l.split(','):
-		lang = lang.strip();
-		weight = 1
-
-		try:
-			(lang, q) = lang.split(';')
-			weight = float(q.split('=')[1])
-		except ValueError:
-			# no weight on that language
-			pass
-
-		langs[lang] = weight
-
-		# if language is something like es-BR, also add es
-		# down the queue with a weight of 0.1
-		if lang.find("-"):
-			baselang = lang.split('-')[0]
-			if baselang not in langs:
-				langs[baselang] = 0.1
-
-	# sort the dict by value and return only the keys back
-	sortedlangs = sorted(langs, key=langs.__getitem__, reverse=True)
-	return sortedlangs
 
 def getUserRealm(ipaddr):
     ip = IP(ipaddr)
