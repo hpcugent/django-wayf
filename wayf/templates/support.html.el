6fcd899da92f4e30f1c9ae820c64a3842c04b40c
Apollon Oikonomopoulos <apollon@noc.grnet.gr>
2009-05-19 17:06:12 +0300

    Fixed support-page translation

--- wayf/templates/support.html.el
+++ wayf/templates/support.html.el
@@ -2,23 +2,17 @@
 {% load i18n %}
 {% block header %}{% trans "User support" %}{% endblock %}
 {% block content %}
-{% spaceless %}
-Παρουσιάστηκε πρόβλημα κατά τη χρήση της Υποδομής Ταυτοποίησης και Εξουσιοδότησης του ΕΔΕΤ. Παρακαλούμε επικοινωνήστε με τον οικείο φορέα σας{% if not idpname %}.{% else %},{% endif %}
+Παρουσιάστηκε πρόβλημα κατά τη χρήση της Υποδομής Ταυτοποίησης και Εξουσιοδότησης του ΕΔΕΤ. Παρακαλούμε επικοινωνήστε με τον οικείο φορέα σας.
 {% if idpname %}
-<strong>{{ idpname }}</strong>
-{% endif %}
-{% if idp.contact.email or idp.contact.telephone %}
-, με έναν από τους παρακάτω τρόπους:
-{% endif %}
-{% endspaceless %}
+Σύμφωνα με την επιλεγμένο φορέα σας, «<strong>{{ idpname }}</strong>», 
+μπορείτε να επικοινωνήσετε με έναν από τους παρακάτω τρόπους:
 <ul>
-
 {% if idp.contact.email %}
-	<li>email: <a href="mailto:{{ idp.contact.email }}?subject={% trans "Shibboleth Authentication Issue" %}">{{ idp.contact.email }}</a></li>
+	<li><b>E-mail:</b> <a href="mailto:{{ idp.contact.email }}?subject={% trans "Shibboleth Authentication Issue" %}">{{ idp.contact.email }}</a></li>
 {% endif %}
 {% if idp.contact.telephone %}
-	<li>{% trans "Telephone" %}: {{ idp.contact.telephone }}</li>
+	<li><b>{% trans "Telephone" %}</b>: {{ idp.contact.telephone }}</li>
 {% endif %}
-
 </ul>
+{% endif %}
 {% endblock %}
