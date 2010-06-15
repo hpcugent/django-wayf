2572e7d2f82dd230daaeec39ca37b23e58b19d2e
Faidon Liambotis <faidon@noc.grnet.gr>
2010-06-15 18:21:38 +0300

    Use a here() lambda for template path in settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for wayf project.
+import os
+here = lambda x: os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -70,7 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/path/to/templates',
+    here('templates'),
 )
 
 INSTALLED_APPS = (
