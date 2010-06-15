e57112f2ffa1698fefb21711f89c6089c24da434
Faidon Liambotis <faidon@noc.grnet.gr>
2010-06-15 20:11:57 +0300

    Adjust urls.py and documentation to create some permanent sensible urls

--- urls.py
+++ urls.py
@@ -20,7 +20,18 @@ urlpatterns += patterns('',
     (r'^debug/?$', 'grnet_aai.aai.views.debug'),
     (r'^setlanguage/(.*)', 'grnet_aai.aai.views.setlanguage'),
     (r'^participants/?$', 'grnet_aai.aai.views.idp_list'),
+    (r'^(registry/.+)/$', 'grnet_aai.aai.views.static'),
     (r'^/?$', 'grnet_aai.aai.views.index'),
-    (r'([^/]+)/?$', 'grnet_aai.aai.views.static'),
 )
 
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^schema/(?P<schema>.+)/$', 'redirect_to',
+        {'url': '/static/%(schema)s.schema', 'permanent': False}),
+    (r'^policy/$', 'redirect_to',
+        {'url': '/static/policy.pdf', 'permanent': False}),
+)
+
+# catch-all
+urlpatterns += patterns('',
+    (r'([^/]+)/?$', 'grnet_aai.aai.views.static'),
+)
