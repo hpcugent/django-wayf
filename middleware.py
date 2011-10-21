a7cb78d6bec211bf708a981f780d7c884a543b03
Faidon Liambotis <faidon@noc.grnet.gr>
2011-10-21 19:14:02 +0300

    Add a HandleExceptionMiddleware to mask IOErrors
    Shamefully stolen^Wborrowed from
    http://stackoverflow.com/questions/3823280/ioerror-request-data-read-error

--- middleware.py
+++ middleware.py
@@ -2,8 +2,19 @@ from grnet_aai.settings import WAYF_SITENAME
 
 class VhostMiddleware:
     def process_request(self,request):
-        if 'HTTP_HOST' in request.META and request.META['HTTP_HOST'] == WAYF_SITENAME:
+        if 'HTTP_HOST' in request.META and \
+            request.META['HTTP_HOST'] == WAYF_SITENAME:
             request.urlconf = 'grnet_aai.wayf.urls'
 
         return None
 
+
+class HandleExceptionMiddleware:
+    def process_exception(self, request, exception):
+        if isinstance(exception, IOError) and \
+           'request data read error' in unicode(exception):
+            logging.info('%s %s: %s: Request was canceled by the client.' % (
+                request.build_absolute_uri(), request.user, exception))
+            return HttpResponseServerError()
+
+        return None
