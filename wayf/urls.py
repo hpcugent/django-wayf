ff24a6c213a28fe4b262bcc38613364173592d01
Jens Timmerman <jens.timmerman@ugent.be>
2016-04-07 17:15:56 +0200

    removed references to aai from wayf

--- wayf/urls.py
+++ wayf/urls.py
@@ -3,19 +3,7 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = []
 
-# Static files
-if settings.DEBUG:
-    urlpatterns += patterns('django.views.static',
-    (r'^static/(?P<path>.*)$', 'serve',
-        {'document_root': settings.MEDIA_ROOT}),
-    (r'^favicon.ico$', 'serve',
-        {'path': "favicon.ico"}),
-    (r'^robots.txt$', 'serve',
-        {'path': "robots.txt"}),
-    )
-
 urlpatterns += patterns('',
     (r'^/?$', 'wayf.views.wayf'),
-    (r'^setlanguage/(.*)', 'aai.views.setlanguage'),
-    (r'^feeds/(.+\.json)$', 'aai.views.json'),
+    (r'^setlanguage/(.*)', 'wayf.views.setlanguage'),
 )
