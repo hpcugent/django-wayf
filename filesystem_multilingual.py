14d804016f137c783541fb2a3f210b220827f1a7
Jens Timmerman <jens.timmerman@ugent.be>
2016-04-07 14:06:53 +0200

    updated filesystem_multilangual to work on django 1.8

--- filesystem_multilingual.py
+++ filesystem_multilingual.py
@@ -5,20 +5,23 @@ It will try to load <template_name>.<language_code> and fall back to <template_n
 
 from django.utils import translation
 from django.template import TemplateDoesNotExist
-from django.template.loaders import filesystem
+from django.template.loaders.filesystem import Loader as BaseLoader
 from django.conf import settings
 
-def load_template_source(template_name, template_dirs=None):
-    try:
-        return filesystem.load_template_source(translation.get_language() + "/" + template_name, template_dirs)
-    except TemplateDoesNotExist:
-        pass
 
-    try:
-        return filesystem.load_template_source(settings.LANGUAGE_CODE + "/" + template_name, template_dirs)
-    except TemplateDoesNotExist:
-        pass
+class Loader(BaseLoader):
 
-    return filesystem.load_template_source(template_name, template_dirs)
+    def load_template_source(self, template_name, template_dirs=None):
+        try:
+            return BaseLoader.load_template_source(self, translation.get_language() + "/" + template_name, template_dirs)
+        except TemplateDoesNotExist:
+            pass
 
-load_template_source.is_usable = True
+        try:
+            return BaseLoader.load_template_source(self, settings.LANGUAGE_CODE + "/" + template_name, template_dirs)
+        except TemplateDoesNotExist:
+            pass
+
+        return BaseLoader.load_template_source(self, template_name, template_dirs)
+
+    load_template_source.is_usable = True
